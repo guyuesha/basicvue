0e5a34af15828c7e40ce20052cf32a14cba7b310
Hu Sha <husha5@126.com>
2017-08-07 23:24:38 +0800

    update: check

--- src/components/foo/check.vue
+++ src/components/foo/check.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="wrapper">
+    <!-- <el-checkbox :label="city" :key="city" class="check" @change="choosedistrict(city)">{{city}}
+        </el-checkbox> -->
     <el-checkbox class="checkAll" :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">华东</el-checkbox>
     <el-checkbox-group class="checkGroup" v-model="checkedCities" @change="handleCheckedCitiesChange">
       <template v-for="city in cities">
-        <el-checkbox :label="city" :key="city" class="check" @change="choosedistrict(city)">{{city}}
-        </el-checkbox>
+        <el-checkbox class="checkAll" 
+        :indeterminate="district[city].isIndeterminate" 
+        v-model="checkedCities" @change="handleCheckCityChange($event, city)">{{city}}</el-checkbox>
         <div class="pop">
           <i :class="['el-icon--right', 'arrow-down', currentSelection!==city?'el-icon-caret-bottom': 'el-icon-caret-top']" @click="showDistrict(city)"></i>
           <div class="popover" v-show="currentSelection===city">
-            <el-checkbox-group class="checkGroup" v-model="checkedDistrict" @change="handleCheckedDistrict">
-              <el-checkbox v-for="pro in district[city]" :label="pro" :key="pro" class="check-sub">
+            <el-checkbox-group class="checkGroup" v-model="district[city].listChecked" @change="handleCheckedDistrict">
+              <el-checkbox v-for="pro in district[city].list" :label="pro" :key="pro" class="check-sub">
                 {{pro}}</el-checkbox>
             </el-checkbox-group>
-
           </div>
         </div>
       </template>
@@ -22,18 +24,38 @@
 
 <script>
   const cityOptions = ['上海', '北京', '广州', '深圳'];
+  // const cityChecked = [true, true, false, false];
   const district = {
-    '上海': ['上海1', '上海2', '上海3'],
-    '北京': ['北京1', '北京2', '北京3'],
-    '广州': ['广州1', '广州2', '广州3'],
-    '深圳': ['深圳1', '深圳2', '深圳3'],
+    '上海': {
+      list: ['上海1', '上海2', '上海3'],
+      listChecked: ['上海1'],
+      checkedAll: false,
+      isIndeterminate: false
+    },
+    '北京': {
+      list: ['北京1', '北京2', '北京3'],
+      listChecked: ['北京1', '北京3'],
+      checkedAll: false,
+      isIndeterminate: false
+    },
+    '广州': {
+      list: ['广州1', '广州2', '广州3'],
+      listChecked: ['广州1', '广州2', '广州3'],
+      checkedAll: true,
+      isIndeterminate: false
+    },
+    '深圳': {
+      list: ['深圳1', '深圳2', '深圳3'],
+      listChecked: ['深圳1', '深圳2', '深圳3'],
+      checkedAll: true,
+      isIndeterminate: false
+    }
   }
   export default {
     data() {
       return {
         checkAll: true,
         checkedCities: ['上海', '北京'],
-        checkedDistrict: ['上海1', '上海2'],
         cities: cityOptions,
         isIndeterminate: true,
         district: Object.assign(district),
@@ -42,14 +64,23 @@
     },
     methods: {
       handleCheckAllChange(event) {
+        console.log('handleCheckAllChange ' , event);
         this.checkedCities = event.target.checked ? cityOptions : [];
         this.isIndeterminate = false;
+        console.log('checked city ',  this.checkedCities);
+      },
+      handleCheckCityChange($event, city) {
+        console.log($event, city);
+        this.district[city].listChecked = event.target.checked ? this.district[city].listChecked : [];
+        this.district[city].isIndeterminate = false;
+        console.log('handleCheckCityChange', this.checkedCities);
+        // this.checkedCities 
       },
       handleCheckedCitiesChange(value) {
         let checkedCount = value.length;
         this.checkAll = checkedCount === this.cities.length;
         this.isIndeterminate = checkedCount > 0 && checkedCount < this.cities.length;
-        console.log('chos pro ', this.checkedCities, 'value', value);
+        console.log('handleCheckedCitiesChange ', this.checkedCities, 'value', value);
 
       },
       handleCheckedDistrict(value) {
@@ -69,6 +100,9 @@
       choosedistrict() {
 
       }
+    },
+    mounted() {
+      console.log(this.district);
     }
   };
 
